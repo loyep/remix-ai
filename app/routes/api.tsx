556c74c985e8b3fb080096389656dbd1fcbebd4d
Zack <lorenx@163.com>
2023-09-11 00:57:30 +0800

    feat: kv duration

--- app/routes/api.tsx
+++ app/routes/api.tsx
@@ -1,11 +1,13 @@
-import { db, schema } from "../server/db";
+// import { db, schema } from "../server/db";
 import { client } from "~/server/redis";
 
 export async function loader() {
-  const res = await db.select().from(schema.users);
-  console.log(res);
+  // const res = await db.select().from(schema.users);
+  // console.log(res);
+  const start = Date.now();
   const value = await client.get('key');
   console.log(value);
+  console.log(Date.now() - start);
   // const redisRes = await client.hGetAll('test');
   // console.log(redisRes);
   return new Response("Hello Remix AI", {
